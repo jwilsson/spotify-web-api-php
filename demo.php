210fbbdd33df96f9138aa8331f158994b1f23003
Jonathan Wilsson <jonathan.wilsson@gmail.com>
2014-06-18 21:48:23 +0200

    Add Artist class

--- demo.php
+++ demo.php
@@ -14,11 +14,20 @@ $session = new Session($config);
 if (isset($_GET['code'])) {
     $session->requestToken($_GET['code']);
 
-    // Get many albums
-    $album = new Album('41MnTivkwTO3UUJ8DrqEJJ');
+    // Get many artists
+    $artist = new Artist();
+    $artists = $artist->getMany(array(
+        '0oSGxfWSnnOXhD2fKuz2Gy',
+        '3dBVyJ7JuOMt4GE9607Qin'
+    ));
+
+    print_r($artists);
+
+    // Get album tracks
+    /*$album = new Album('41MnTivkwTO3UUJ8DrqEJJ');
     $tracks = $album->getTracks();
 
-    print_r($tracks);
+    print_r($tracks);*/
 
     // Get the current user's info
     /*$response = Request::api('GET', 'v1/me', array(), array(
