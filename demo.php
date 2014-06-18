34d2e2f5d4323fdcd2ef53745e2086e867573cf0
Jonathan Wilsson <jonathan.wilsson@gmail.com>
2014-06-18 21:25:45 +0200

    Update demo

--- demo.php
+++ demo.php
@@ -13,25 +13,23 @@ $session = new Session($config);
 
 if (isset($_GET['code'])) {
     $session->requestToken($_GET['code']);
-    $response = '';
 
-    // Get tracks from an album
-    $album = new Album();
+    // Get many albums
+    /*$album = new Album();
     $albums = $album->getMany(array(
         '41MnTivkwTO3UUJ8DrqEJJ',
         '6JWc4iAiJ9FjyK0B59ABb4',
         '6UXCm6bOO4gFlDQZV5yL37'
     ));
 
-    print_r($albums);
-    die();
+    print_r($albums);*/
 
     // Get the current user's info
     /*$response = Request::api('GET', 'v1/me', array(), array(
         'Authorization' => 'Bearer ' . $session->getAccessToken()
-    ));*/
+    ));
 
-    print_r($response);
+    print_r($response);*/
 } else {
     header('Location: ' . $session->getAuthorizeURL('user-read-email'));
 }
