82855661ecb77e038909f5ba3293496864fc0227
Jonathan Wilsson <jonathan.wilsson@gmail.com>
2014-10-26 12:38:55 +0100

    Update tests for non-static Request methods

--- tests/RequestTest.php
+++ tests/RequestTest.php
@@ -3,16 +3,23 @@ use \SpotifyWebAPI;
 
 class RequestTest extends PHPUnit_Framework_TestCase
 {
+    private $request = null;
+
+    public function setUp()
+    {
+        $this->request = new SpotifyWebAPI\Request();
+    }
+
     public function testApi()
     {
-        $response = SpotifyWebAPI\Request::api('GET', '/v1/albums/7u6zL7kqpgLPISZYXNTgYk');
+        $response = $this->request->api('GET', '/v1/albums/7u6zL7kqpgLPISZYXNTgYk');
 
         $this->assertObjectHasAttribute('id', $response['body']);
     }
 
     public function testApiParameters()
     {
-        $response = SpotifyWebAPI\Request::api('GET', '/v1/albums', array(
+        $response = $this->request->api('GET', '/v1/albums', array(
             'ids' => '1oR3KrPIp4CbagPa3PhtPp,6lPb7Eoon6QPbscWbMsk6a'
         ));
 
@@ -22,14 +29,14 @@ class RequestTest extends PHPUnit_Framework_TestCase
 
     public function testSend()
     {
-        $response = SpotifyWebAPI\Request::send('GET', 'https://api.spotify.com/v1/albums/7u6zL7kqpgLPISZYXNTgYk');
+        $response = $this->request->send('GET', 'https://api.spotify.com/v1/albums/7u6zL7kqpgLPISZYXNTgYk');
 
         $this->assertObjectHasAttribute('id', $response['body']);
     }
 
     public function testSendParameters()
     {
-        $response = SpotifyWebAPI\Request::send('GET', 'https://api.spotify.com/v1/albums', array(
+        $response = $this->request->send('GET', 'https://api.spotify.com/v1/albums', array(
             'ids' => '1oR3KrPIp4CbagPa3PhtPp,6lPb7Eoon6QPbscWbMsk6a'
         ));
 
@@ -39,14 +46,14 @@ class RequestTest extends PHPUnit_Framework_TestCase
 
     public function testSendHeaders()
     {
-        $response = SpotifyWebAPI\Request::send('GET', 'https://api.spotify.com/v1/albums/7u6zL7kqpgLPISZYXNTgYk');
+        $response = $this->request->send('GET', 'https://api.spotify.com/v1/albums/7u6zL7kqpgLPISZYXNTgYk');
 
         $this->assertInternalType('string', $response['headers']);
     }
 
     public function testSendStatus()
     {
-        $response = SpotifyWebAPI\Request::send('GET', 'https://api.spotify.com/v1/albums/7u6zL7kqpgLPISZYXNTgYk');
+        $response = $this->request->send('GET', 'https://api.spotify.com/v1/albums/7u6zL7kqpgLPISZYXNTgYk');
 
         $this->assertEquals(200, $response['status']);
     }
