120749d67aef73adca852122d63b56d948181a74
Jonathan Wilsson <jonathan.wilsson@gmail.com>
2017-03-31 19:07:29 +0200

    Restore demo

--- demo.php
+++ demo.php
@@ -5,9 +5,9 @@ ini_set('display_errors', 1);
 require 'vendor/autoload.php';
 
 $session = new SpotifyWebAPI\Session(
-    '40eba578acc44b5cae8680ccd5542b4f',
-    '6b3bf52a554e4f788ae5259699bb8b60',
-    'http://localhost:8888/spotify-web-api-php/demo.php'
+    'CLIENT_ID',
+    'CLIENT_SECRET',
+    'REDIRECT_URI'
 );
 
 $api = new SpotifyWebAPI\SpotifyWebAPI();
@@ -16,15 +16,12 @@ if (isset($_GET['code'])) {
     $session->requestAccessToken($_GET['code']);
     $api->setAccessToken($session->getAccessToken());
 
-    print_r($api->getMyCurrentPlaybackInfo());
+    print_r($api->me());
 } else {
     $scopes = [
         'scope' => [
             'user-read-email',
             'user-library-modify',
-            'user-read-playback-state',
-            'user-modify-playback-state',
-            'user-read-currently-playing',
         ],
     ];
 
