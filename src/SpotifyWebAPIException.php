7cadab4e40e7fc32228814444be47cd59d005e31
Doeke Norg <doekenorg@users.noreply.github.com>
2018-11-07 18:55:10 +0100

    Added helper functions to Exceptions for easier handling. (#142)
    * Added helper functions to Exceptions for easier handling.
    
    * Added some test coverage
    
    * Added more coverage. Must make tests pass!
    
    * Change quotes

--- src/SpotifyWebAPIException.php
+++ src/SpotifyWebAPIException.php
@@ -3,5 +3,14 @@ namespace SpotifyWebAPI;
 
 class SpotifyWebAPIException extends \Exception
 {
-    // dummy
+    const TOKEN_EXPIRED = 'The access token expired';
+
+    /**
+     * Returns if the exception was thrown because of an expired token.
+     * @return bool
+     */
+    public function hasExpiredToken()
+    {
+        return $this->getMessage() === self::TOKEN_EXPIRED;
+    }
 }
