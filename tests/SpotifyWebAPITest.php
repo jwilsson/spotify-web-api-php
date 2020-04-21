4d9e5719ba4c7e9863696b7e4256b26c01f9e8c8
John Wedgbury <johnw@wedgbury.net>
2020-04-21 22:21:45 +0100

    Add tests

--- tests/SpotifyWebAPITest.php
+++ tests/SpotifyWebAPITest.php
@@ -2278,6 +2278,31 @@ class SpotifyWebAPITest extends PHPUnit\Framework\TestCase
         $this->assertTrue($response);
     }
 
+    public function testQueue()
+    {
+        $headers = [
+            'Authorization' => 'Bearer ' . $this->accessToken,
+        ];
+
+        $return = [
+            'status' => 204,
+        ];
+
+        $stub = $this->setupStub(
+            'POST',
+            '/v1/me/player/queue',
+            [],
+            $headers,
+            $return
+        );
+
+        $api = new SpotifyWebAPI\SpotifyWebAPI([], null, $stub);
+        $api->setAccessToken($this->accessToken);
+        $response = $api->queue('spotify:track:6ek0XS2AUbzrHS0B5wPNcU');
+
+        $this->assertTrue($response);
+    }
+
     public function testReorderPlaylistTracks()
     {
         $options = [
